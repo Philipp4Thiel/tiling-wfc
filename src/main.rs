6dce7f2b059c30133d537d321e7193ad36985c58
Philipp <philipp.thiel@inf.ethz.ch>
2022-08-14 00:27:51 +0200

    did stuff

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,62 @@ extern crate rand;
 extern crate raylib;
 
 mod wave_funcion_collapse {
-    use rand::rngs::ThreadRng;
+    use ndarray::{s, Array2, Array3};
+    use rand::{rngs::ThreadRng, seq::SliceRandom};
     use raylib::prelude::*;
 
-    pub type Coordinates = [usize; 2];
+    type Coordinates = (usize, usize);
+    type Entropy = usize;
+    type EntropyField = Array2<Entropy>;
+    type WaveField = Array3<bool>;
+
+    struct Tileset {}
+
+    impl Tileset {
+        fn from_file() -> Self {
+            todo!()
+        }
+    }
+
+    fn generate_wave_field(tileset: &Tileset) -> WaveField {
+        todo!()
+    }
+
+    fn generate_entropy_field(wave_field: &WaveField) -> EntropyField {
+        fn entropy((x, y): (usize, usize), wave_field: &WaveField) -> Entropy {
+            wave_field
+                .slice(s![x, y, ..])
+                .iter()
+                .filter(|&&b| b)
+                .count()
+        }
+
+        let wave_field_shape: &[usize] = wave_field.shape();
+        let entropy_shape: (usize, usize) = (wave_field_shape[0], wave_field_shape[1]);
+        let entropy_field: EntropyField =
+            Array2::from_shape_fn(entropy_shape, |(x, y)| entropy((x, y), wave_field));
+        entropy_field
+    }
+
     pub struct WaveFunction {
         done: bool,
+        tileset: Tileset,
+        entropy_field: EntropyField,
+        wave_field: WaveField,
     }
 
     impl WaveFunction {
         pub fn from_file() -> Self {
-            todo!()
+            let tileset: Tileset = Tileset::from_file();
+            let wave_field: WaveField = generate_wave_field(&tileset);
+            let entropy_field: EntropyField = generate_entropy_field(&wave_field);
+
+            Self {
+                done: false,
+                tileset,
+                entropy_field,
+                wave_field,
+            }
         }
 
         pub fn done(&self) -> bool {
@@ -21,10 +66,21 @@ mod wave_funcion_collapse {
         }
 
         fn get_min_entropy(&self, rng: &mut ThreadRng) -> Coordinates {
-            todo!()
+            let min = self.entropy_field.iter().min().unwrap();
+            *self
+                .entropy_field
+                .indexed_iter()
+                .filter(|(_, v)| *v == min)
+                .map(|v| v.0)
+                .collect::<Vec<(usize, usize)>>()
+                .choose(rng)
+                .unwrap()
         }
 
         pub fn collapse(&self, rng: &mut ThreadRng) -> () {
+            if self.done {
+                return;
+            }
             let coords: Coordinates = self.get_min_entropy(rng);
             todo!()
         }
@@ -60,7 +116,6 @@ fn get_config() -> Config {
 fn main() {
     let mut rng: ThreadRng = thread_rng();
     let wave_function = WaveFunction::from_file();
-
     let config: Config = get_config();
 
     let &debug = config.get("debug").unwrap_or(&false);
